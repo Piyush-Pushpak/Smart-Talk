5f1210fb4604c295a54b005877a08f22ff0171df
Piyush-Pushpak <piyush592005@gmail.com>
2025-01-20 22:52:12 +0530

    db connect & user register done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-import dotenv from "dotenv";
-dotenv.config()
+import 'dotenv/config';
 import http from 'http';
 import app from './app.js';
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
